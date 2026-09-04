f80c1d5814073407b57f72d74be621c4cd1d26e8
jehanusama <jehanusama3@gmail.com>
2026-09-04 21:19:58 +0300

    Remove mock data

--- src/data/mockCategories.js
+++
@@ -1,6 +0,0 @@
-export const mockCategories = [
-  { id: "c1", name: "Paper Bags" },
-  { id: "c2", name: "Plastic Bags" },
-  { id: "c3", name: "Gift Bags" },
-  { id: "c4", name: "Jute Bags" }
-];
